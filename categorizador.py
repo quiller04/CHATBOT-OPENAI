a765044066e12d9ff597dbd11fa1400436dd6b8c
quiller04 <guilhermequillerdealencar@gmail.com>
2025-02-25 11:32:39 -0300

    feat: add interação usuario com GPT categorizando

--- categorizador.py
+++ categorizador.py
@@ -4,26 +4,46 @@ import os
 
 load_dotenv()
 cliente  = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+pronpt_sistema = """
+    Você é um categorizador de produtos
+    voce deve assumir as categorias presentes na lista abaixo.
+
+    #Lista de categorias validas
+    -moda sustentavel
+    -produtos para lar
+    -beleza natural
+    -eletrõnicos verdes
+    -higiene individual
+
+    #formato de saída
+    produto: Nome do produto
+    Categoria: apresenta a categoria do produto
+
+    # Exemplo de saída
+    produto: escova elétrica com recarga solar
+    Categoria: Eletrônicos Verdes
+
+"""
+
+promot_usuario =  input("apresente o nome de um produto: ")
 
 resposta = cliente.chat.completions.create(
     messages=[
         {
             "role" : "system",
-            "content" : """
-            classifique o produto abaixo em uma das categorias: higiene pessoal, moda ou casa e de uma descrição da categoria.
-            """
+            "content" : pronpt_sistema
         },
         {
             "role" : "user",
-            "content" : """
-            escova de dentes de bambu
-            """
+            "content" : promot_usuario
         }
     ],
-    model="gpt-4",
+    model=modelo,
     temperature = 0,
     max_tokens=200,
-    n = 3 #determina a quantidade de respostas
+    #n = 3 #determina a quantidade de respostas
 )
 
 print(resposta.choices[0].message.content)
